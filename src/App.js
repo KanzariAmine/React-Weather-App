f4fbef4f9df412f46685422bb12b822f08632152
Mahmoud Amine Kanzari <kanzari.amin@gmail.com>
2018-08-02 20:54:38 +0100

    Create App copmonent and Titles Component

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Titles from './components/Titles'
+
+//Begin the Class Component
+class App extends Component{
+
+  //Begin Render Method
+  render(){
+    return(
+      <div>
+      <Titles />
+      </div>
+    );
+  }
+  //End Render Method
+}; 
+//End Class
+
+export default App
---
+++ src/components/Titles.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+//Begin Class Titles Component
+class Titles extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Weather Finder</h1>
+        <p>Find out Temperation, condition and more...</p>
+      </div>
+    );
+  }
+};
+//End Class Titles Component
+
+export default Titles;
